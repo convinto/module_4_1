23b9101ef420d04579b71419275feee773646389
convinto <convinto@yandex.ru>
2024-07-26 23:56:04 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+from true_math import divide as dtm
+from fake_math import divide as dfm
+
+dtm(10, 0)
+dfm(5, 0)
+
+result1 = dfm(69, 3)
+result2 = dfm(3, 0)
+result3 = dtm(49, 7)
+result4 = dtm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
